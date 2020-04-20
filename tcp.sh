efd27b438115dca7c619a050aa083d536a0daaa1
jeffern-1 <64008983+jeffern-1@users.noreply.github.com>
2020-04-20 18:25:00 +0800

    Add files via upload

--- tcp.sh
+++ tcp.sh
@@ -8,15 +8,7 @@ export PATH
 #	Version: 1.3.1
 #	Author: Jeffern
 #	Twitter:@jeffern12
-#=================================================
 
-sh_ver="1.3.1"
-github="raw.githubusercontent.com/cx9208/Linux-NetSpeed/master"
-
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 #安装BBRplus内核
 installbbrplus(){
 	kernel_version="4.14.129-bbrplus"
